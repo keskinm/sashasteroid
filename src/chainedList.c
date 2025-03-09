d99a96bea0692b49111999aace76253cca03d6f5
Sasha Keskin <skeskin@bwi.earth>
2025-03-09 22:01:47 +0100

    add chainedList

---
+++ src/chainedList.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void addNode(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+int main() {
+    struct Node* head = NULL;
+    addNode(&head, 10);
+    addNode(&head, 20);
+
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+
+    return 0;
+}
